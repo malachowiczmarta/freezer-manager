43d46e8843483e6134448d47dd4460273ea0c54f
Marta Małachowicz <marta@exefs-macbook-pro.home>
2021-02-23 14:35:39 +0100

    home style

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,18 +1,21 @@
-import React from 'react';
-import styles from './Home.module.scss';
-import {ReactComponent as WomenImg} from "../../assets/img/OpenDoodles.svg"
+import React from "react";
+import styles from "./Home.module.scss";
+import { ReactComponent as WomenImg } from "../../assets/img/OpenDoodles.svg";
 
 function Home() {
   return (
     <div className={styles.wrapper}>
-        <div className={styles.main}>
-            <div className={styles.header}>
-                <h1>store food safely<br/>in the freezer</h1>
-                <h2>Control what and how long you keep in the freezer.</h2>
-            </div>
-            <WomenImg />
+      <div className={styles.main}>
+        <div className={styles.header}>
+          <h1>
+            store food safely
+            <br />
+            in the freezer
+          </h1>
+          <h2>Control what and how long you keep in the freezer.</h2>
         </div>
-
+        <WomenImg />
+      </div>
     </div>
   );
 }
