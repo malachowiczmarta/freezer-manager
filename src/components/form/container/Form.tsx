39802aff2d7121254652258a31d588a1f82f3d03
Marta Małachowicz <marta@exefs-macbook-pro.home>
2021-03-08 13:23:51 +0100

    form validation

--- src/components/form/container/Form.tsx
+++ src/components/form/container/Form.tsx
@@ -11,6 +11,32 @@ const initialFormState = {
   date: "",
 };
 
+const validate = (values: any) => {
+  console.log("-=-=-=-", values);
+  let errors = {};
+  console.log(values.name && values.name.length);
+  console.log(values.name.length);
+
+  if (!values.name) {
+    (errors as any).name = "Product name is required!";
+  } else if (values.name.length <= 4) {
+    (errors as any).name = "Product name is too short.";
+  } else {
+    (errors as any).name = "";
+  }
+
+  if (!values.date) {
+    (errors as any).date = "Date is required!";
+  }
+
+  if (!values.category) {
+    (errors as any).category = "Product category is required!";
+  }
+
+  console.log("****", errors);
+  return errors;
+};
+
 const Form = () => {
   const [isOpen, setIsOpen] = useState(false);
   const [formValues, setFormValues] = useState(initialFormState);
@@ -20,48 +46,17 @@ const Form = () => {
     setIsOpen(!isOpen);
   };
 
-  const validateField = (form: any) => {
-    if (!form.name) {
-      setFormErrors({
-        ...formErrors,
-        name: "Product name is required",
-      });
-      return;
-    } else if (form.name.length < 2) {
-      setFormErrors({
-        ...formErrors,
-        name: "Product name is too short",
-      });
-      return;
-    } else {
-      setFormErrors({
-        ...formErrors,
-        name: "",
-      });
-    }
-
-    if (!form.date) {
-      setFormErrors({
-        ...formErrors,
-        date: "Date is required",
-      });
-      return;
-    } else {
-      setFormErrors({
-        ...formErrors,
-        date: "",
-      });
-    }
-    return null;
-  };
-
   const updateField = (e: any) => {
     setFormValues({
       ...formValues,
       [e.target.name]: e.target.value,
     });
 
-    validateField(formValues);
+    // let formErrors: any = validate(formValues);
+    // setFormErrors({
+    //   ...formErrors,
+    //   formErrors,
+    // });
   };
 
   const handleCategoryClick = (e: any) => {
@@ -74,9 +69,14 @@ const Form = () => {
 
   const handleAddProduct = (e: any) => {
     e.preventDefault();
+    let formErrors: any = validate(formValues);
+    setFormErrors({
+      ...formErrors,
+      formErrors,
+    });
     let productName = e.target.elements.name.value;
     let date = e.target.elements.date.value;
-    console.log(date);
+    console.log(formValues);
     console.log(`Name is: ${productName}, date is: ${date}`);
   };
 
@@ -100,17 +100,20 @@ const Form = () => {
         onChange={updateField}
         error={formErrors.date}
       />
-      <Dropdown
-        onClick={toggleDd}
-        open={isOpen}
-        variant="categories"
-        label={formValues.category ? formValues.category : "Product category"}
-      >
-        <CategoryList
-          onClick={handleCategoryClick}
-          value={formValues.category}
-        />
-      </Dropdown>
+      <div className={styles.ddContainer}>
+        <Dropdown
+          onClick={toggleDd}
+          open={isOpen}
+          variant="categories"
+          label={formValues.category ? formValues.category : "Product category"}
+        >
+          <CategoryList
+            onClick={handleCategoryClick}
+            value={formValues.category}
+          />
+        </Dropdown>
+        <span className={styles.errorMsg}>{formErrors.category}</span>
+      </div>
       <Button variant="add" />
     </form>
   );
